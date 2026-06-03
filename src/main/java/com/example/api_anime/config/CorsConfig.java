ad432109925e4a46274d0a1ab53dd8f010642527
git-felipeUrtubia <felipe.urtubia7mail.com>
2026-06-03 19:45:20 -0400

    problemas resueltos configuracion cors

--- src/main/java/com/example/api_anime/config/CorsConfig.java
+++ src/main/java/com/example/api_anime/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
@@ -16,7 +18,7 @@ public class CorsConfig {
 
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOrigin("https://anime-rest-api-production.up.railway.app");
 
         corsConfiguration.setAllowCredentials(true);
 
